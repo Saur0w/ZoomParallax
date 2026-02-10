c7fe3d05c2109093544cb660a956e949353034f3
saur0w <sthapliyal085@gmail.com>
2026-02-10 10:19:41 +0530

    "Structure"

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from './style.module.scss';
+import { gsap } from 'gsap';
+import { useGSAP } from "@gsap/react";
+
+export default function Landing() {
+    return (
+        <section className={styles.landing}>
+
+        </section>
+    );
+}
