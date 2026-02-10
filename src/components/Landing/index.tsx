bbc5aa4a9a6ef1a21c7d75691af8d7033cdab94b
saur0w <sthapliyal085@gmail.com>
2026-02-10 10:28:44 +0530

    "Credit"

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -3,6 +3,7 @@
 import styles from './style.module.scss';
 import { gsap } from 'gsap';
 import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 export default function Landing() {
     return (
