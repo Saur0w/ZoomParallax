a526a60055fe0aa246f4f25a92cdb9c5a42ff398
saur0w <sthapliyal085@gmail.com>
2026-02-11 15:21:05 +0530

    "Final Commit"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,26 @@
 
 import styles from "./page.module.css";
 import Landing from "../components/Landing/index";
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis'
 
 export default function Home() {
-  return (
+    useEffect(() => {
+        const lenis = new Lenis();
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
+    return (
     <div className={styles.page}>
       <Landing />
     </div>
